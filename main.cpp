e845f3297015fa553fcf273a5618d45d5a856634
Avitus Valence <avitusvalence6@gmail.com>
2024-12-06 15:59:28 +0300

    project initialisation

--- main.cpp
+++ main.cpp
@@ -1,44 +1,45 @@
 #include<iostream>
 #include<fstream>
+#include <string>
 
 using namespace std;
 
-class temp{
-    string studentName,studentId,staffName,staffId;
+class temp {
+    string studentName,studentId,staffId,staffName;
     string course;
     fstream file;
 
-public:
+public :
     void addStudent();
     void addStaff();
 };
+
 int main(){
+    temp system;
     char choice;
 
-
-    cout<<".................Choose the action.\n";
-    cout<<"1.Add student\n";
-    cout<<"2.Add staff\n";
-    cout<<"3.Exit\n";
-    cin>>choice;
+while(true) {
+    cout << ".................Choose the action.\n";
+    cout << "1.Add student\n";
+    cout << "2.Add staff\n";
+    cout << "3.Exit\n";
+    cin >> choice;
+    cin.ignore();
 
     switch (choice) {
         case'1':
-
+            system.addStudent();
             break;
         case '2':
-
+            system.addStaff();
             break;
         case '3':
-
+            return 0;
             break;
         default:
-            cout<<"Incorrect choice!";
-
-
+            cout<<"Incorrect choice!\n";
     }
-
-
+}
 
     return 0;
 }
@@ -50,12 +51,16 @@ void temp ::addStudent() {
     cout<<"\n Enter the course of student::";
     getline(cin,course);
 
-    file.open("student system.txt",ios :: out | ios::app);
-    file<<studentId<<"*"<<studentName<<"*"<<course<<endl;
-    file.close();
-
+    file.open("studentSystem.txt",ios :: out | ios::app);
+    if (file.is_open()){
+        file<<studentId<<"*"<<studentName<<"*"<<course<<endl;
+        file.close();
+    }else{
+        cout<<"Error opening file\n";
+    }
 }
 
 void temp::addStaff() {
 
-}
+    }
+
