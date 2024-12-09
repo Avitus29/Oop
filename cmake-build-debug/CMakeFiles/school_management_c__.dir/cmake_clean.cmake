de6a8d5db3b07f84309f2586f9f25a71ecb4fa31
Avitus Valence <avitusvalence6@gmail.com>
2024-12-09 23:50:52 +0300

    Display staff attendance

---
+++ cmake-build-debug/CMakeFiles/school_management_c__.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/school_management_c__.dir/main.cpp.obj"
+  "CMakeFiles/school_management_c__.dir/main.cpp.obj.d"
+  "libschool_management_c__.dll.a"
+  "school_management_c__.exe"
+  "school_management_c__.exe.manifest"
+  "school_management_c__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/school_management_c__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
